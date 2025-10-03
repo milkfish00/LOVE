ced4116355cbc66c0a36ba492be3bab721b17fd8
“milkfish00” <satorijal@gmail.com>
2025-10-03 19:02:12 -0400

    button

--- app/components/DisableDraftMode.tsx
+++ app/components/DisableDraftMode.tsx
@@ -1,35 +1,3 @@
-"use client";
-
-import { useTransition } from "react";
-import { useRouter } from "next/navigation";
-import { disableDraftMode } from "@/app/actions";
-
 export function DisableDraftMode() {
-  const router = useRouter();
-  const [pending, startTransition] = useTransition();
-
-  // Only show the disable draft mode button when outside of Presentation Tool
-  const isInsidePresentationTool =
-    typeof window !== "undefined" && window.top !== window.self;
-  if (isInsidePresentationTool) {
-    return null;
-  }
-
-  const disable = () =>
-    startTransition(async () => {
-      await disableDraftMode();
-      router.refresh();
-    });
-
-  return (
-    <div>
-      {pending ? (
-        ""
-      ) : (
-        <button type="button" onClick={disable}>
-    
-        </button>
-      )}
-    </div>
-  );
+  return null;
 }
