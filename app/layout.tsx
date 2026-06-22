2755bdaa4a3a506f40f7fcad31bcb3e9dd23017c
“milkfish00” <satorijal@gmail.com>
2026-06-22 12:39:54 -0400

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { openSans } from "@/app/fonts";
 import { sanityClient } from "@/app/lib/sanity";
@@ -130,6 +131,18 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '855718100531348');
+fbq('track', 'PageView');`}
+        </Script>
         <link rel="preconnect" href="https://cdn.sanity.io" crossOrigin="" />
         <link rel="dns-prefetch" href="//cdn.sanity.io" />
         <link
@@ -143,7 +156,18 @@ export default async function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={openSans.className}>{children}</body>
+      <body className={openSans.className}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=855718100531348&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
